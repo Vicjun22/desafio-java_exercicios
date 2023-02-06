00a9d63b5538d7311998c45a2004963e95a0ae56
Victor Ross <vicjun22@gmail.com>
2023-02-06 19:52:01 -0300

    exercicio 1 fundamentos

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import fundamentos.Fundamentos;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Fundamentos fundamentos = new Fundamentos();
+
+        int mediaAritmetica1 = (int) (Math.random() * 10) + 1;
+        int mediaAritmetica2 = (int) (Math.random() * 10) + 1;
+        int mediaAritmetica3 = (int) (Math.random() * 10) + 1;
+
+        int response = fundamentos.exercicio1(mediaAritmetica1, mediaAritmetica2, mediaAritmetica3);
+        int mediaFinal = (mediaAritmetica1 + mediaAritmetica2 + mediaAritmetica3) / 3;
+
+        if (mediaFinal == response) {
+            System.out.println("A resposta está correta!");
+        } else {
+            System.out.printf("Sua resposta foi %d e deveria ser %d", response, mediaFinal);
+        }
+
+    }
+}
---
+++ src/fundamentos/Fundamentos.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class Fundamentos {
+
+    // Encontre a média aritmética de 3 valores inteiros distindos vindos por parâmetro.
+    // O resultado deverá coincidir com o resultado mostrado no terminal.
+    // O resultado deverá ser também um número inteiro.
+    public int exercicio1(int a, int b, int c) {
+        int resultado = 0;
+
+        // escreva seu código abaixo:
+
+
+        return resultado;
+    }
+}
